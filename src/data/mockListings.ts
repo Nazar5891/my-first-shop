938eefcce3eaab02070488d810c68462dd57147e
Rokitne <nazar0111111@gmail.com>
2026-08-12 18:14:27 +0300

    Remove all demo listings from initial catalog

--- src/data/mockListings.ts
+++ src/data/mockListings.ts
@@ -1,20 +1,9 @@
 import { Listing } from '../types';
-import { DOMESTIC_SERVICES } from './servicesDomestic';
-import { TECHNICAL_SERVICES } from './servicesTechnical';
-import { CONSTRUCTION_SERVICES } from './servicesConstruction';
-import { TRANSPORT_SERVICES } from './servicesTransport';
-import { RIDESHARE_LISTINGS } from './rideshareListings';
-import { SALES_LISTINGS } from './salesListings';
 
 // Center coordinates: Rokytne, Rivne Oblast (51.2825, 27.2130)
 export const COMMUNITY_CENTER: [number, number] = [51.2825, 27.2130];
 export const COMMUNITY_NAME = "смт Рокитне та Рокитнівська громада";
 
-export const INITIAL_LISTINGS: Listing[] = [
-  ...RIDESHARE_LISTINGS,
-  ...SALES_LISTINGS,
-  ...DOMESTIC_SERVICES,
-  ...TECHNICAL_SERVICES,
-  ...CONSTRUCTION_SERVICES,
-  ...TRANSPORT_SERVICES,
-];
+// На старті каталог порожній: користувачі додають власні оголошення.
+// Демонстраційні оголошення навмисно не завантажуються.
+export const INITIAL_LISTINGS: Listing[] = [];
