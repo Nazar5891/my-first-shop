8f3871b392a625f126c6e60bc3a93d493e74b835
Rokitne <nazar0111111@gmail.com>
2026-08-12 19:31:16 +0300

    Add Firebase client configuration

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFhw3i0Xa6_UeN7fjSMAHxS7kcfbQbuMo',
+  authDomain: 'rokitne--yahmyrov.firebaseapp.com',
+  projectId: 'rokitne--yahmyrov',
+  storageBucket: 'rokitne--yahmyrov.firebasestorage.app',
+  messagingSenderId: '582905785754',
+  appId: '1:582905785754:web:a76b3d35c704aa5e125e23',
+  measurementId: 'G-BQ5B18N7T2'
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
